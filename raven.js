25aaa1204dc05efe433b29383a79610583bd9956
McrayNick <nickhunter509@gmail.com>
2025-01-08 13:18:04 +0300

    Update raven.js

--- raven.js
+++ raven.js
@@ -702,6 +702,57 @@ const fetch = require('node-fetch');
   }
 }
 	  break;
+		      case 'playdoc': {
+		      const yts = require("yt-search");
+const ffmpeg = require("fluent-ffmpeg");
+const fs = require("fs");
+const path = require("path");
+const axios = require("axios");
+
+    try {
+        if (!text) return m.reply("What song do you want to download?");
+
+        let search = await yts(text);
+        let link = search.all[0].url;
+
+        let data = await fetchJson(`https://api.dreaded.site/api/ytdl/audio?url=${link}`);
+        let videoUrl = data.result.downloadLink;
+
+        let outputFileName = `${search.all[0].title}.mp3`;
+        let outputPath = path.join(__dirname, outputFileName);
+
+       
+        const response = await axios({
+            url: videoUrl,
+            method: "GET",
+            responseType: "stream"
+        });
+
+        
+        ffmpeg(response.data)
+            .toFormat("mp3")
+            .save(outputPath)
+            .on("end", async () => {
+                await client.sendMessage(
+                    m.chat,
+                    {
+                        document: { url: outputPath },
+                        mimetype: "audio/mp3",
+                        fileName: outputFileName,
+                    },
+                    { quoted: m }
+                );
+                fs.unlinkSync(outputPath);
+            })
+            .on("error", (err) => {
+                m.reply("Download failed\n" + err.message);
+            });
+
+    } catch (error) {
+        m.reply("Download failed\n" + error.message);
+    }
+} 
+break;
 		      
 	      case 'metallic': {
 		      var mumaker = require("mumaker");
